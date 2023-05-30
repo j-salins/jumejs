b58b1a3b3de0c03951a898a07ef95d10f5e8ab6e
Janis Salins <janis.salins@gmail.com>
2023-05-30 21:38:24 +0300

    first ver

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    ".dist/index.html",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'pr-green': '#002B2C',
+        'pr-dark-green': '#002425',
+        'pr-green-accent': '#97D100',
+        'pr-beige': '#DBDCC9'
+      }
+    },
+  },
+  plugins: [],
+}
+
